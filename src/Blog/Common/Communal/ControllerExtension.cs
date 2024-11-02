f044253bc0bb150514959e8c00035b5c17a4943b
Sajad Jalilian <sajadjdev@gmail.com>
2024-11-02 11:43:47 +0330

    Add ControllerExtension.cso

---
+++ src/Blog/Common/Communal/ControllerExtension.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Common.Communal;
+
+public static class ControllerExtension
+{
+    public static IActionResult ReturnResponse(this ControllerBase controller, OperationResult operation)
+    {
+        object response = operation.Value;
+        if (response is ErrorModel errorModel)
+            response = new ErrorResponse(errorModel);
+
+        return operation.Status switch
+        {
+            OperationResultStatus.Ok => controller.Ok(response),
+            OperationResultStatus.Created => controller.Created("-", response), // URI can not be null. I will get fixed in .NET 8
+            OperationResultStatus.InvalidRequest => controller.BadRequest(response),
+            OperationResultStatus.NotFound => controller.NotFound(response),
+            OperationResultStatus.Unauthorized => controller.UnprocessableEntity(response),
+            OperationResultStatus.Unprocessable => controller.UnprocessableEntity(response),
+            _ => controller.UnprocessableEntity(response)
+        };
+    }
+
+    public static IActionResult ReturnResponseModel(this ControllerBase controller, OperationResult operation)
+    {
+        var response = operation.Value;
+        if (response is ErrorModel errorModel)
+            response = new ErrorResponse(errorModel);
+
+        return operation.Status switch
+        {
+            OperationResultStatus.Ok => controller.Ok(response),
+            OperationResultStatus.Created => controller.Created("-", response), // URI can not be null. I will get fixed in .NET 8
+            OperationResultStatus.InvalidRequest => controller.BadRequest(response),
+            OperationResultStatus.NotFound => controller.NotFound(response),
+            OperationResultStatus.Unauthorized => controller.UnprocessableEntity(response),
+            OperationResultStatus.Unprocessable => controller.UnprocessableEntity(response),
+            _ => controller.UnprocessableEntity(response)
+        };
+    }
+}
