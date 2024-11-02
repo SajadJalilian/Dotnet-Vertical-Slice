6f362d801e4b0570dd6a447f021d7000cc7ed09f
Sajad Jalilian <sajadjdev@gmail.com>
2024-11-02 11:45:52 +0330

    Remove redundant code

--- src/Blog/Common/Communal/ControllerExtension.cs
+++ src/Blog/Common/Communal/ControllerExtension.cs
@@ -21,22 +21,4 @@ public static class ControllerExtension
             _ => controller.UnprocessableEntity(response)
         };
     }
-
-    public static IActionResult ReturnResponseModel(this ControllerBase controller, OperationResult operation)
-    {
-        var response = operation.Value;
-        if (response is ErrorModel errorModel)
-            response = new ErrorResponse(errorModel);
-
-        return operation.Status switch
-        {
-            OperationResultStatus.Ok => controller.Ok(response),
-            OperationResultStatus.Created => controller.Created("-", response), // URI can not be null. I will get fixed in .NET 8
-            OperationResultStatus.InvalidRequest => controller.BadRequest(response),
-            OperationResultStatus.NotFound => controller.NotFound(response),
-            OperationResultStatus.Unauthorized => controller.UnprocessableEntity(response),
-            OperationResultStatus.Unprocessable => controller.UnprocessableEntity(response),
-            _ => controller.UnprocessableEntity(response)
-        };
-    }
 }
--- src/Blog/Common/Communal/WebApplicationExtension.cs
+++ src/Blog/Common/Communal/WebApplicationExtension.cs
@@ -19,22 +19,4 @@ public static class WebApplicationExtension
             _ => Results.UnprocessableEntity(response)
         };
     }
-
-    public static IResult ReturnResponseModel(this WebApplication app, OperationResult operation)
-    {
-        var response = operation.Value;
-        if (response is ErrorModel errorModel)
-            response = new ErrorResponse(errorModel);
-
-        return operation.Status switch
-        {
-            OperationResultStatus.Ok => Results.Ok(response),
-            OperationResultStatus.Created => Results.Created("-", response), // URI can not be null. I will get fixed in .NET 8
-            OperationResultStatus.InvalidRequest => Results.BadRequest(response),
-            OperationResultStatus.NotFound => Results.NotFound(response),
-            OperationResultStatus.Unauthorized => Results.UnprocessableEntity(response),
-            OperationResultStatus.Unprocessable => Results.UnprocessableEntity(response),
-            _ => Results.UnprocessableEntity(response)
-        };
-    }
 }
